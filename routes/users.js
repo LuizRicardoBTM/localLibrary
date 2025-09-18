bce990a8c62989fcb139c1ff264a8982c0b3eae9
Luiz Ricardo <luizricardo.btm@gmail.com>
2025-09-18 15:20:36 -0300

    cool page added

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(red, res, next){
+  res.send('You are so cool! You made it!')
+});
+
 module.exports = router;
