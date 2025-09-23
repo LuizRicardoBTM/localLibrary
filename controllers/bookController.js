64167ed2c2b319a735c7f51669c67c0a1ccff5ab
Luiz Ricardo <luizricardo.btm@gmail.com>
2025-09-23 16:39:51 -0300

    Created and stylized book list page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -29,7 +29,12 @@ exports.index = async (req, res, next) => {
 };
 
 exports.book_list = async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Book list");
+  const allBooks = await Book.find({}, "title author")
+    .sort({ title: 1 })
+    .populate("author")
+    .exec();
+
+  res.render("book_list", { title: "Book List", book_list: allBooks });
 };
 
 exports.book_detail = async (req, res, next) => {
