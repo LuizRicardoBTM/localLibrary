fe3859fa299f1c29d2418289145975b99bdc4473
Luiz Ricardo <luizricardo.btm@gmail.com>
2025-09-24 09:18:33 -0300

    added genre details path and page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,5 @@
 const Genre = require("../models/genre");
+const Book = require("../models/book");
 
 exports.genre_list = async (req, res, next) => {
     const allGenres = await Genre.find().sort({ name: 1 }).exec();
@@ -9,7 +10,22 @@ exports.genre_list = async (req, res, next) => {
 };
 
 exports.genre_details = async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+    const [genre, booksInGenre] = await Promise.all([
+        Genre.findById(req.params.id).exec(),
+        Book.find({genre: req.params.id}, "Title Sumarry").exec(),
+    ]);
+
+    if (genre === null){
+        const err = new Error("Genre not found");
+        err.st5atus(404);
+        return next(err);
+    }
+
+    res.render("genre_detail", {
+        title: "Genre detail",
+        genre,
+        genre_books: booksInGenre,
+    })
 };
 
 exports.genre_create_get = async (req, res, next) => {
