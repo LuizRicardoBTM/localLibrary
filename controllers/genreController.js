89f3d33d0272e641cdac450a7db5c597556c418f
Luiz Ricardo <luizricardo.btm@gmail.com>
2025-09-24 09:25:58 -0300

    visual improvements

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -10,14 +10,15 @@ exports.genre_list = async (req, res, next) => {
 };
 
 exports.genre_details = async (req, res, next) => {
+    
     const [genre, booksInGenre] = await Promise.all([
         Genre.findById(req.params.id).exec(),
         Book.find({genre: req.params.id}, "Title Sumarry").exec(),
     ]);
 
-    if (genre === null){
+    if(genre === null){
         const err = new Error("Genre not found");
-        err.st5atus(404);
+        err.status = 404;
         return next(err);
     }
 
